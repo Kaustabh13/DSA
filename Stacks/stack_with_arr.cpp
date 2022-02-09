5006061cdf07adaa06281f05a2942939866ccb3b
Kaustabh <47168538+Kaustabh13@users.noreply.github.com>
2022-02-09 20:43:57 +0530

    Parenthesis Matching Function

--- Stacks/stack_with_arr.cpp
+++ Stacks/stack_with_arr.cpp
@@ -7,7 +7,7 @@ class Stack {
 public:
     int size;
     int top;
-    int arr[];
+    char arr[];
 };
 
 // Check Size
@@ -44,16 +44,15 @@ bool isFull(Stack* stack) {
  * @param val -> The new value to be inserted in the stack
  */
 
-void push(Stack* stack, int val) {
+void push(Stack* stack, char val) {
 
     if(isFull(stack)) {
         cout << "Stack Overflow! Stack is full." << endl;
     } else {
         stack->top++;
         stack->arr[stack->top] = val;
-        
-        cout << "Pushed at position " << stack->top <<  " -> " << val << endl;
 
+        cout << "Pushed at position " << stack->top <<  " -> " << val << endl;
     }
 }
 
@@ -66,18 +65,17 @@ void push(Stack* stack, int val) {
  * @return int -> The popped value
  */
 
-int pop(Stack* stack) {
+char pop(Stack* stack) {
 
-    int popped = -1;
+    char popped = '-';
     if(isEmpty(stack)) {
         cout << "Stack Underflow! Stack is empty." << endl;
     } else {
-        int val = stack->arr[stack->top];
+        char val = stack->arr[stack->top];
         stack->top--;
         popped = val;
         
         cout << "The popped item is -> " << popped << endl;
-
     }
 
     return popped;
@@ -104,6 +102,58 @@ void peek(Stack* stack, int position) {
 }
 
 /**
+ * @brief This function is used to match the characters/parenthesis
+ * 
+ * @param pre -> The pushed value
+ * @param post -> The target value
+ * @return -> A boolean which tells if characters matched or not
+ */
+
+bool matchChar(char pre, char post) {
+
+    if(pre == '(' && post == ')' || pre == '[' && post == ']' || pre == '{' && post == '}') {
+        return true;
+    }
+
+    return false;
+}
+
+//Parenthesis Matching
+
+/**
+ * @brief This function is used to check whether the parenthesis is matching or not
+ * 
+ * @param s -> The string with equation/expression
+ * @return -> A boolean which tells whether the parenthesis is matching or not
+ */
+
+bool isParenthesisMatching(string s) {
+
+    Stack * sp = new Stack();
+    sp->size = s.length();
+    sp->top = -1;
+
+    for(int i = 0; i < s.length(); i++) {
+
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            push(sp, s[i]);
+        } else if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+
+           if(isEmpty(sp)) { return false; }
+           
+           char popVal = pop(sp);
+
+           if(!matchChar(popVal, s[i])) {
+               return false;
+           }
+        }
+    }
+
+    return isEmpty(sp);
+}
+
+
+/**
  * @brief This main function acting as initializer
  * 
  * @return int 
@@ -111,22 +161,28 @@ void peek(Stack* stack, int position) {
 
 int main() {
 
-    Stack * sp = new Stack();
-    sp->size = 6;
-    sp->top = -1;
+    // Stack * sp = new Stack();
+    // sp->size = 6;
+    // sp->top = -1;
 
-    push(sp, 1);
-    push(sp, 2);
-    push(sp, 3);
+    // push(sp, 'a');
+    // push(sp, 'b');
+    // push(sp, 'c');
 
-    cout << endl;
-    pop(sp);
-    cout << endl;
+    // cout << endl;
+    // pop(sp);
+    // cout << endl;
   
-    for(int i = 1; i <= sp->top + 1; i++) {
-        peek(sp, i);
-    }
+    // for(int i = 1; i <= sp->top + 1; i++) {
+    //     peek(sp, i);
+    // }
 
+    string exp = "8-12-1]+{1-10})";
+    if (isParenthesisMatching(exp)) {
+        cout << "Patenthesis is matching" << endl;
+    } else {
+        cout << "Patenthesis is not matching" << endl;
+    }
 
     return 0;
 }
